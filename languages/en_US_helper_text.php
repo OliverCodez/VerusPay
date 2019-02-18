9a762c1dbf6ffceeda5dc2749db991b8a36e01f6
J Oliver Westbrook <ethadvisor@gmail.com>
2019-02-18 17:12:28 -0500

    Add files via upload

---
+++ languages/en_US_helper_text.php
@@ -0,0 +1,85 @@
+<?php 
+/**
+ * Plugin Name: VerusPay Verus Gateway
+ * Plugin URI: https://github.com/joliverwestbrook/VerusPay/
+ * Description: Accept Verus Coin (VRSC) cryptocurrency in your online WooCommerce store for physical or digital products.
+ * Author: J Oliver Westbrook
+ * Author URI: https://github.com/joliverwestbrook/
+ * Copyright: (c) 2019 John Oliver Westbrook (johnwestbrook@pm.me)
+ * Version: 0.1.0
+ * Text Domain: wc-gateway-veruspay
+ * Domain Path: /i18n/languages/
+ *
+ * @package   WC-Gateway-Verus
+ * @author    John Westbrook
+ * @category  Admin
+ * @copyright Copyright (c) 2019, John Oliver Westbrook
+ * 
+ * ====================
+ * 
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2019 John Oliver Westbrook
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * 
+ * ====================
+ *
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+// Defines all helper text in en_US - use as $wc_veruspay_text_helper['']
+global $wc_veruspay_text_helper;
+$wc_veruspay_text_helper = array(
+	'method_desc' => 'Allows purchases to be made using Verus Coin (VRSC) cryptocurrency.',
+	'refresh' => 'Refresh',
+	'price' => 'Price',
+	'exchange_rate' => 'Exchange Rate',
+	'awaiting_payment' => 'Awaiting VRSC payment',
+	'payment_button' => 'Proceed to Verus Payment',
+	'severe_error' => 'Error! Please Choose A Different Payment Method or Contact the Store Owner.',
+	'order_processing' => 'Order Complete',
+	'total_in' => 'Total in ',
+	'check_status' => 'Check Status',
+	'price_powered_by' => 'Powered by VerusPay API',
+	'title_on_hold' => 'Please Send Exactly ',
+	'title_ordered' => 'Order Pending Payment',
+	'title_pending' => 'Order Pending Confirmations',
+	'title_paid'	=> 'Payment Received, Awaiting Confirmations',
+	'title_completed' => 'Your Order is Complete!',
+	'title_cancelled' => 'This Order Has Been Cancelled',
+	'msg_sapling_tooltip' => 'Check to enable privacy payment using a "zs" Verus Sapling address. If you pay from a "zs" address to the store "zs" address, you\'ll receive your order confirmation via a private message at your Verus Sapling address.',
+	'msg_valid_for' => 'Valid for',
+	'msg_noscript_refresh' => 'Please click Refresh above to ensure you have the most up-to-date price before your purchase.',
+	'msg_send_on_purchase' => 'Upon purchase send ',
+	'msg_order_cancel_timeout' => 'Payment not received within time limit of ',
+	'msg_payment_amount' => 'Payment Amount',
+	'msg_your_order_num' => 'Your order number ',
+	'msg_has_cancelled_reason' => ' has been cancelled. ',
+	'msg_thank_payment_of' => 'Thank you, your payment of ',
+	'msg_verus_received' => ' VRSC has been received!',
+	'msg_modal_gen_addr' => 'Generating Unique VRSC Payment Address',
+	'msg_modal_update_price' => 'Updating Price Data',
+	'msg_sapling_label' => 'Enable Verus Privacy',
+	'msg_waiting_payment' => 'Waiting for payment, please send within ',
+	'msg_confirming_payment' => 'Waiting for full block confirmations. Progress: ',
+	
+);
+
