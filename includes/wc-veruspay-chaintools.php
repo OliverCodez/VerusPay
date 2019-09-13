855a2362266fb55be510ffb0c8bb18c358487d71
Oliver Westbrook <ethadvisor@gmail.com>
2019-09-13 12:47:36 -0400

    Debugging

--- includes/wc-veruspay-chaintools.php
+++ includes/wc-veruspay-chaintools.php
@@ -59,7 +59,7 @@ function wc_veruspay_go( $chaindc, $url, $chain, $method, $params = NULL ) {
         'c' => $chain,
         'm' => $method,
         'p' => $params, // Passed as json_encode string of correct array layout of parameters in question
-        'o' => null,
+        'o' => null, // TODO: Usage?
     );
     // Pass method and params to VCT API and get return
     $response = wp_remote_post( $url, array(
