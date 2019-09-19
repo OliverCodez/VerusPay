46063cb7346c7180645335a0cc7a5c90c9d5505e
Oliver Westbrook <ethadvisor@gmail.com>
2019-09-19 10:16:10 -0400

    Debugging

--- admin/js/wc-veruspay-admin-scripts.js
+++ admin/js/wc-veruspay-admin-scripts.js
@@ -158,13 +158,13 @@ jQuery( function( $ ) {
 				$('#wc_veruspay_update_iframe').attr('src', url+'?vp=1&update='+code);
 			}
 		});
-		$('#wc_veruspay_update_modal-container_close').click(function(e) {
+		$('#wc_veruspay_update_modal-container_close').on( 'click', function(e) {
 			var urllen = $('#wc_veruspay_update_iframe').attr('src').length;
+			console.log(urllen);
 			$( '#wc_veruspay_update_modal' ).fadeOut();
 			$( '#wc_veruspay_update_code').val('');
 			$( '#wc_veruspay_update_modal-go' ).attr( 'data-url', '' );
 			$('#wc_veruspay_update_iframe').attr('src', 'http://').delay(1000).queue( function( next ) {
-				console.log(urllen);
 				if ( urllen > 7 ) {
 					location.reload();
 				}
